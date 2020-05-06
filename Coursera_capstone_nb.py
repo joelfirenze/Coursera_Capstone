92752dc52681644ded888209b088ace9113fbcf5
eddiechoo <joelfirenze@users.noreply.github.com>
2020-05-06 18:01:46 +0800

    Add files via upload

---
+++ Coursera_capstone_nb.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is meant to be the capstone notebook for the Coursera course
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
